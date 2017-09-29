36675f8b786900fb24a64ef4d0d114a7464576ec
Tim Freeman <d.timothy.freeman@gmail.com>
2017-09-29 14:06:47 -0400

    added category filter to yelp rest-client call

--- app/controllers/api/v1/things_controller.rb
+++ app/controllers/api/v1/things_controller.rb
@@ -17,10 +17,11 @@ class Api::V1::ThingsController < ApplicationController
   def fetch_things
     country = params[:country]
     city = params[:city]
+    filter = params[:filter]
     query_string = "#{city}+#{country}"
     response = RestClient::Request.execute(
       method: :get,
-      url: "https://api.yelp.com/v3/businesses/search?term=attractions&location=#{query_string}",
+      url: "https://api.yelp.com/v3/businesses/search?term=#{filter}&location=#{query_string}",
       headers: {Authorization: "Bearer S1CWwH36GRcefckPGGLSsDjD8xjkV7hyS9VVKTv4D_4BFpOYAweu227a98oM0i0U718dnaYCLdiWt-aRvMR2rwZoT0eIjHkR3TwBj3bmWfQbnFKi064G2tz8jz_OWXYx"}
       )
     things = JSON.parse(response)
